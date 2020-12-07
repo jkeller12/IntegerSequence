823197c80afd02cdb3276d66afad1102fc955d77
Jonah Keller <jkeller10@stuy.edu>
2020-12-07 09:22:01 -0500

    I think it's done

--- Range.java
+++ Range.java
@@ -9,24 +9,28 @@ public class Range implements IntegerSequence
 
   public Range(int start, int end)
   {
-    start = start;
-    end = end;
-    current = start; 
+    this.start = start;
+    this.end = end;
+    this.current = start;
 
   }
 
   public void reset()
   {
+    current = start;
+
 
   }
 
   public int length()
   {
+    return end - start;
 
   }
 
   public boolean hasNext()
   {
+    return (current >= start && current <= end);
     // When current isn't a valid element in teh range, it should
     // return false.
 
@@ -37,6 +41,11 @@ public class Range implements IntegerSequence
     // Throws NoSuchElementException when hasNext() is false
     // This will return the current value, and increase current
     // value by 1.
+    if(!hasNext())
+    {
+      throw new NoSuchElementException();
+    }
+    return current++;
 
 
   }
