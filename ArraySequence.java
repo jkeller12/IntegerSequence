5046bee1a29b8d8d4323ebe3952a1cef3ec451d7
Jonah Keller <jkeller10@stuy.edu>
2020-12-08 11:37:06 -0500

    I copied over next and replaced current with current index.

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,9 +1,10 @@
 import java.util.NoSuchElementException;
-public class ArraySequence implements IntegerSequence{
+public class ArraySequence implements IntegerSequence
+{
   private int currentIndex;
   private int[] data;
 
-  /*Construct the sequence by copying values from the other array into the data array*/
+  //Construct the sequence by copying values from the other array into the data array
   public ArraySequence(int[] other)
   {
     for(int i = 0; i < other.length; i++)
@@ -11,9 +12,29 @@ public class ArraySequence implements IntegerSequence{
       data[i] = other[i];
     }
   }
-  boolean hasNext() ;//does the sequence have more elements?
-  int next();         //return the current value in the sequence and advances to the next element
-  int length();     //returns the total length of the sequence
-  void reset();     //start over from the start of the sequence
 
+  pubic boolean hasNext
+  {
+    return (current >= 0 && current <= data.length);
+  } //does the sequence have more elements?
+
+  public int next()
+  {
+    if(!hasNext())
+    {
+      throw new NoSuchElementException("woopd");
+    }
+    return data[currentIndex++];
   }
+
+  public int length()
+  {
+
+  }     //returns the total length of the sequence
+
+  public void reset()
+  {
+
+  }     //start over from the start of the sequence
+
+}
